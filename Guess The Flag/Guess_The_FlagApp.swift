03332e4810ea6e2b11d2ba8658fc35c38e894f37
AndresGutierrez <c0dingw.dre@gmail.com>
2022-03-08 12:45:04 -0800

    Initial Commit

---
+++ Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Andres Gutierrez on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
