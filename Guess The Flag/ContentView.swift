fad645e19b6e59a8fe52ffebedfc0fc57284e88b
AndresGutierrez <c0dingw.dre@gmail.com>
2022-03-10 00:27:29 -0800

    spruce up Ui

--- Guess The Flag/ContentView.swift
+++ Guess The Flag/ContentView.swift
@@ -18,24 +18,32 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
             VStack (spacing: 40) {
+                Spacer()
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
                     Text(countries[correctAnswer])
                         .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
+                Spacer()
                 ForEach(0..<3) { number in
                     Button {
                         checkAnswer(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .shadow(radius: 5)
                     }
+                    
                 }
+                Spacer()
             }
         }
         .alert(scoreTitle, isPresented: $showScore) {
