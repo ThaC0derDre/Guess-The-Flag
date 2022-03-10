e688e5eb7738a765e5e00c09d2698dae26d29930
AndresGutierrez <c0dingw.dre@gmail.com>
2022-03-10 00:04:04 -0800

    add functionality to game add score reset q & a's

--- Guess The Flag/ContentView.swift
+++ Guess The Flag/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var countries = ["Estonia","France","Germany","Ireland","Italy","Monaco","Nigeria","Poland","Russia", "Spain","UK","US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var showScore        = false
+    @State private var scoreTitle       = ""
+    @State private var scoreLabel       = 0
+    @State private var correctAnswer    = Int.random(in: 0...2)
+    @State private var countries = ["Estonia","France","Germany","Ireland","Italy","Monaco","Nigeria","Poland","Russia", "Spain","UK","US"].shuffled()
+    
     
     var body: some View {
         ZStack {
@@ -26,7 +30,7 @@ struct ContentView: View {
                 }
                 ForEach(0..<3) { number in
                     Button {
-                        
+                        checkAnswer(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
@@ -34,6 +38,31 @@ struct ContentView: View {
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showScore) {
+            Button("OK") {
+                shuffleCountries()
+            }
+        }message: {
+            Text("Score is \(scoreLabel)")
+        }
+    }
+    
+    
+    func checkAnswer(_ number: Int) {
+        scoreTitle = correctAnswer == number ? "correct" : "Wrong"
+        if correctAnswer  == number {
+            scoreTitle = "Correct"
+            scoreLabel += 1
+        }else {
+            scoreTitle = "Wrong"
+        }
+        showScore = true
+    }
+    
+    
+    func shuffleCountries() {
+        correctAnswer = Int.random(in: 0...2)
+        countries.shuffle()
     }
 }
 
